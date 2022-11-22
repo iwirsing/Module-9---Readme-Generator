003e8a71006fe32d96bb79f8b65aeb0e7ba9f10a
iwirsing <ivymolina@gmail.com>
2022-11-22 11:42:02 -0600

    add functionality, able to create a very basic readme file

--- index.js
+++ index.js
@@ -1,15 +1,64 @@
 // TODO: Include packages needed for this application
 const fs=require('fs');
-const inquire=require('inquire');
+const inquirer=require('inquirer');
+const generateMarkdown=require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: ' What is the title of your project?',
+        name:'title',
+
+    },
+    {
+        type: 'input',
+        message: 'Write a description of your project:',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What is the installation instruction to run this project?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What is the usage information for your project?',
+        name: 'usage',
+    },
+    {
+        type: 'list',
+        message: 'What is the usage information for your project?',
+        choices: ['Apache License 2.0', 'MI'T],
+        name:'license',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName,data,(error) =>
+        error? console.log(error):console.log('Successfully generated '+fileName+ ' !'));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        console.log(data);
+        
+        //make sure readme title has capitalized first letter
+        let dataTitle=data.title.split(' ');
+        let newTitle='';
+        for (let word of dataTitle){
+            word=word[0].toUpperCase()+word.substr(1);
+            console.log(word);
+            newTitle +=word;
+        }
+        
+        let fileName='README_'+newTitle+'.md';
+        writeToFile(fileName, generateMarkdown(data));
+    });
+}
 
 // Function call to initialize app
 init();
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,6 +14,18 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## <u> Description:</u>
+  
+  ${data.description}
+
+  ##  Table of Contents 
+  1. [Installation](#installation)
+
+  ## <u> Installation </u>
+
+  ${data.installation}
+
+
 `;
 }
 
